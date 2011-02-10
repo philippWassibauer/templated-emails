594a3824dbcb7e482475c2e63f34645077012892
Philipp Wassibauer <phil@maptales.com>
2011-02-10 18:33:38 +0100

    templated_emails

---
+++ templated_emails/utils.py
@@ -0,0 +1,21 @@
+from django.core.mail import EmailMultiAlternatives
+from django.conf import settings
+from django.template import Template, Context
+from django.contrib.sites.models import Site
+from django.template.loader import render_to_string
+
+def send_html_email(recipients, template_path, context={},
+                    from_email=settings.DEFAULT_FROM_EMAIL):
+    current_site = Site.objects.get(id=settings.SITE_ID)
+    context["current_site"] = current_site
+    context["STATIC_URL"] = settings.STATIC_URL
+    
+    context = Context(context)
+    subject = render_to_string("%s/short.txt"%template_path, context)\
+                                .replace('\n', '').replace('\r', '')
+    text = render_to_string("%s/email.txt"%template_path, context)
+    body = render_to_string("%s/email.html"%template_path, context)
+    msg = EmailMultiAlternatives(subject, text, from_email, recipients)
+    msg.attach_alternative(body, "text/html")
+    msg.send()
+
