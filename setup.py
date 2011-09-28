c9c4cc210d22f4894105898887edfeacd1fc3c70
Philipp Wassibauer <phil@maptales.com>
2011-09-28 16:34:12 +0300

    fixing setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,7 @@
-import os
-import sys
-from distutils.core import setup
-from django.core.management.commands.compilemessages import compile_messages
+#!/usr/bin/env python
+from setuptools import setup
 
-
-license_text = open('LICENSE.txt').read()
 long_description = open('README.rst').read()
-
 
 setup(
     name = 'templated-emails',
@@ -14,16 +9,15 @@ setup(
     url = 'https://github.com/philippWassibauer/templated-emails',
     author = "Philipp Wassibauer",
     author_email = "phil@gidsy.com",
-    license = license_text,
+    license = 'MIT License',
     packages = ['templated_emails'],
-    data_files=[('', ['LICENSE.txt', 'README.rst'])],
     description = 'Like django-notifications, but just for sending plain emails. Written because it is ennoying to fork other apps just to make an email into an HTML email',
     long_description=long_description,
     classifiers = ['Development Status :: 4 - Beta',
                    'Environment :: Web Environment',
                    'Framework :: Django',
                    'Intended Audience :: Developers',
-                   'License :: OSI Approved :: BSD License',
+                   'License :: OSI Approved :: MIT License',
                    'Topic :: Internet :: WWW/HTTP :: Dynamic Content'],
     install_requires=[
         'pynliner',
