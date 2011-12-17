cb129266b84d4fe61037480ff1cf8cf000d00f9e
Philipp Wassibauer <phil@gidsy.com>
2011-12-17 22:19:02 +0100

    minor style fixes

--- templated_emails/views.py
+++ templated_emails/views.py
@@ -16,39 +16,42 @@ def index(request, template_name="templated_emails/index.html"):
 
 
 def view(request, path, template_name="templated_emails/view.html"):
-    # get extends node
-    # get all block nodes
-    # do this recursive until no more extends
-    # then place html in blocks
-    old_template_debug_setting = settings.TEMPLATE_DEBUG
-    rendered_subject = ""
-    rendered_html = ""
-    rendered_text = ""
-    try:
-        settings.TEMPLATE_DEBUG = True
+    if not request.user.is_superuser:
+        raise Http404
+    else:
+        # get extends node
+        # get all block nodes
+        # do this recursive until no more extends
+        # then place html in blocks
+        old_template_debug_setting = settings.TEMPLATE_DEBUG
+        rendered_subject = ""
+        rendered_html = ""
+        rendered_text = ""
         try:
-            template = get_template("emails%s/email.html"%path)
-            rendered_html = recursive_block_replace(template, {})
-        except:
-            pass # there might be no html
+            settings.TEMPLATE_DEBUG = True
+            try:
+                template = get_template("emails%s/email.html"%path)
+                rendered_html = recursive_block_replace(template, {})
+            except:
+                pass # there might be no html
 
-        try:
-            template = get_template("emails%s/email.txt"%path)
-            rendered_text = recursive_block_replace(template, {})
-        except:
-            pass # there might be no text file
+            try:
+                template = get_template("emails%s/email.txt"%path)
+                rendered_text = recursive_block_replace(template, {})
+            except:
+                pass # there might be no text file
 
-        try:
-            template = get_template("emails%s/short.txt"%path)
-            rendered_subject = recursive_block_replace(template, {})
-        except: 
-            pass # there might be no html
+            try:
+                template = get_template("emails%s/short.txt"%path)
+                rendered_subject = recursive_block_replace(template, {})
+            except:
+                pass # there might be no html
 
-    finally:
-        settings.TEMPLATE_DEBUG = old_template_debug_setting
+        finally:
+            settings.TEMPLATE_DEBUG = old_template_debug_setting
 
-    return render_to_response(template_name, {
-        "rendered_subject": rendered_subject,
-        "rendered_html": rendered_html,
-        "rendered_text": rendered_text,
-    }, context_instance=RequestContext(request))
+        return render_to_response(template_name, {
+            "rendered_subject": rendered_subject,
+            "rendered_html": rendered_html,
+            "rendered_text": rendered_text,
+        }, context_instance=RequestContext(request))
